1972d70ec4393034fa7e6b4027c6c1c4d7e78e29
Halepensis <antgranada@gmail.com>
2025-10-10 10:41:49 +0200

    feat: add ButtonPrimario component and refactor Input component to use Material-UI TextField

--- Frontend/src/components/common/Input/Input.tsx
+++ Frontend/src/components/common/Input/Input.tsx
@@ -1,15 +1,12 @@
 import './Input.css';
 
+import TextField from '@mui/material/TextField';
+
 type InputProps = {
   label: string;
   placeholder?: string;
 };
 
-export const Input = ({ label, placeholder }: InputProps) => {
-  return (
-    <div className="input-container">
-      <label>{label}</label>
-      <input type="text" placeholder={placeholder} />
-    </div>
-  );
+export const Input = ({ label }: InputProps) => {
+  return <TextField id="outlined-basic" label={label} variant="outlined" />;
 };
