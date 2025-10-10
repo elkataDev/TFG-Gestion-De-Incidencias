92193fc0d39e6b0fd8eaff3198c297069a101729
Halepensis <antgranada@gmail.com>
2025-10-10 09:52:24 +0200

    feat: add AppPrueba component and configure environment for playground mode

---
+++ Frontend/src/AppPrueba.tsx
@@ -0,0 +1,9 @@
+import { Input } from './components/common/Input/Input';
+
+export const AppPrueba = () => {
+  return (
+    <>
+      <Input label="Nombre" placeholder="Escribe tu nombre" />
+    </>
+  );
+};
--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
+import { AppPrueba } from './AppPrueba';
 import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
@@ -9,10 +10,11 @@ if (!rootElement) {
   throw new Error('Root element not found');
 }
 
+// Cambia según quieras probar o ejecutar la app normal
+const USE_PLAYGROUND = import.meta.env.VITE_USE_PLAYGROUND === 'true';
+
 createRoot(rootElement).render(
   <BrowserRouter>
-    <StrictMode>
-      <App />
-    </StrictMode>
+    <StrictMode>{USE_PLAYGROUND ? <AppPrueba /> : <App />}</StrictMode>
   </BrowserRouter>
 );
