5d95335cc6aab5c8595c92bf81323f9cc640b534
Halepensis <antgranada@gmail.com>
2025-10-10 10:02:16 +0200

    chore: ignorar AppPrueba.tsx para modificaciones locales

--- Frontend/src/AppPrueba.tsx
+++
@@ -1,9 +0,0 @@
-import { Input } from './components/common/Input/Input';
-
-export const AppPrueba = () => {
-  return (
-    <>
-      <Input label="Nombre" placeholder="Escribe tu nombre" />
-    </>
-  );
-};
