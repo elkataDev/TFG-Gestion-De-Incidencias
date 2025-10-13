a5f173d9d3ffcc1883a34282bf4e64d5c6e2694f
Halepensis <antgranada@gmail.com>
2025-10-13 17:07:26 +0200

    feat: add LoginForm component and include modern-normalize CSS for consistent styling

--- Frontend/src/main.tsx
+++ Frontend/src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import "./styles/modern-normalize.css"
 import App from './App';
 import { AppPrueba } from './AppPrueba';
 import { BrowserRouter } from 'react-router-dom';
+import
 
 const rootElement = document.getElementById('root');
 
