0a956e5b2e348ad1498708a0d302480ed600dbf9
Halepensis <antgranada@gmail.com>
2025-10-21 10:27:41 +0200

    feat: implement LoginForm component and associated styles, and remove unused files

---
+++ Frontend/src/pages/Login/LoginPage.tsx
@@ -0,0 +1,9 @@
+import { LoginForm } from '@/components/features/usuarios/LoginForm/LoginForm';
+import './LoginPage.css';
+export const LoginPage = () => {
+  return (
+    <main>
+      <LoginForm />
+    </main>
+  );
+};
